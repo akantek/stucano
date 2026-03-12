5a2ca059c41fc6def191f6f0e1dda8bb31261983
Antonio Kantek <antonio.kantek@gmail.com>
2026-03-12 14:52:59 -0700

    Place the stack in a safe, standardized location

--- src/main.asm
+++ src/main.asm
@@ -1,4 +1,7 @@
 main:
+  di
+  ld sp, $f380  ; ROM standard SP initialization (move to top-of-RAM)
+
   call boot
   jr demo
 
@@ -17,7 +20,8 @@ demo:
   ; if (frame_count == 60) {beep! && frame_count = 0}
   ld a, (hl)
   cp 60
-  jr nz, .vblank_trace_end
+  jr nz, .game_loop
+
   ld (hl), 0
   call BEEP
   jr .game_loop
@@ -39,7 +43,6 @@ boot:
   xor a
   ld (CLIKSW), a  ; Set Click Switch ($F3DB) to 0
 
-  di
   call install_vblank_hook
   ei
   ret
